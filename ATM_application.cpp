c33f1a54b8f46fdca3103f34e072253612802441
Saurav <karkisaurav145@gmail.com>
2023-01-16 16:28:07 +0200

    Basic application ompleted

--- ATM_application.cpp
+++ ATM_application.cpp
@@ -7,16 +7,48 @@ void showMenu() {
     cout << "1. Check balance" << endl;
     cout << "2. Deposit" << endl;
     cout << "3. WithDraw" << endl;
-    cout << "*************************";
+    cout << "4. Exit"<< endl;
+    cout << "*************************" << endl;
 
 }
 int main(){
     //check balance, deposit, withdraw, show menu
 
     int option;
+    double balance = 500;
+
+    do{
     showMenu();
     cout << "Option: ";
     cin >> option;
+    system("cls");
+
+
+    switch(option){
+        case 1:
+        cout << "The Balance is  " << balance << endl; break;
+
+        case 2: 
+        cout << "Deposit amount: ";
+        double deposit;
+        cin >> deposit;
+        balance += deposit;
+        break;
+
+        case 3:
+        cout << "WithDraw Amount: ";
+        double withdraw;
+        cin >> withdraw;
+
+        if(withdraw <= balance){
+        balance -= withdraw;
+        }else{
+            cout << "Not enough Money " << endl;
+        }
+        break;
+    }
+
+    }while(option != 4);
 
     return 0;
 }
